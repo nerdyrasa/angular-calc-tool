c030eb4908dbe1a0488e9db989dd1cc09adfdbb8
nerdyrasa <nerdyrasa@users.noreply.github.com>
2017-01-03 07:00:43 -0600

    Set default input values.

--- assets/calculator.js
+++ assets/calculator.js
@@ -6,6 +6,9 @@
 
     $scope.lumen_options = [375, 600, 900, 1125, 1600];
     $scope.current_lumens = 600;
+    $scope.current_cost = 12;
+    $scope.current_hours = 3;
+    $scope.total_days = 365;
 
   }]);
 
